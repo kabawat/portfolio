28150c47ab8772b26e0951d52af7e4c89058da82
w3App <kshatriyakabawat@gmail.com>
2023-07-23 15:44:56 +0530

    Update Skills.js

--- src/components/home/Skills.js
+++ src/components/home/Skills.js
@@ -27,7 +27,7 @@ const Skills = () => {
             <Row>
                 {
                     skills?.map((item, keys) => {
-                        return <Col xxl={3} xl={3} lg={3} md={4} sm={6} xs={12} className='py-4' data-aos="fade-up" data-aos-delay={`${keys * 100}`}>
+                        return <Col xxl={3} xl={3} lg={3} md={4} sm={6} xs={12} key={keys} className='py-4' data-aos="fade-up" data-aos-delay={`${keys * 100}`}>
                             <div className="skils-container d-flex align-items-center">
                                 <div className="skill-icon-box">
                                     <div className="skill-icon">
