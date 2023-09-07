1c96d878116cd65ffcac02efeb93af531b35b30a
w3App <kshatriyakabawat@gmail.com>
2023-09-07 12:24:39 +0530

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import About from '@/components/home/About'
 import Skills from '@/components/home/Skills'
 import AboutSection from '@/components/home/about-senction'
 import Hero from '@/components/home/hero'
+import Image from 'next/image'
 import React from 'react'
 import { Container } from 'react-bootstrap'
 
@@ -24,6 +25,9 @@ const Home = () => {
           <About />
         </div>
         <div className="py-5 skills">
+          <div className="bg">
+            <Image src="/bg/blumba.png" width={1920} height={1080} />
+          </div>
           <Container>
             <h2 className='text-center' data-aos="zoom-in-up" data-aos-delay="0">Development Skills</h2>
             <Skills />
