0b80f36f22c9cb11cccb5d2b1d718dd8b94a9581
kabawat <118162167+kabawat@users.noreply.github.com>
2024-03-15 18:10:30 +0530

    Update hero.js

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -10,11 +10,11 @@ const Hero = () => {
         <div className="hero-section d-flex justify-content-center pt-100 align-items-center flex-column">
             <div className="hero-intro">
                 <h2 className='text-center' data-aos="zoom-in-up" data-aos-delay="0">Hi There!</h2>
-                <h1 className='text-center py-2' data-aos="zoom-in" data-aos-delay="200">Welcome To My <span>Portfolio!</span></h1>
-                <h3 className='text-center' data-aos="zoom-in-up" data-aos-delay="400">
+                <h1 className='text-center py-2' data-aos="zoom-in" data-aos-delay="100">Welcome To My <span>Portfolio!</span></h1>
+                <h3 className='text-center' data-aos="zoom-in-up" data-aos-delay="200">
                     Web Application <span>|</span> Game Development
                 </h3>
-                <div className="about-social" data-aos="zoom-in-up" data-aos-delay="400">
+                <div className="about-social" data-aos="zoom-in-up" data-aos-delay="300">
                     <div className="d-flex justify-content-center">
                         <button className={`cta ${isMedia && 'isMedia'}`} onClick={() => setIsMedia(!isMedia)}>
                             <span>Follow me</span>
